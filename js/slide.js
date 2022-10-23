ec7640adbee13d4ae93f2f358e19861daa02fce2
miss <ddc.akari@gmail.com>
2022-10-23 21:19:45 +0900

    UNIVERSE

---
+++ js/slide.js
@@ -0,0 +1,23 @@
+/* section UNIVERSE 슬라이드 컨트롤 */
+
+const swiper = new Swiper(".UNIVERSE .swiper", {
+  direction: "horizontal", // 수평 슬라이드
+  autoplay: {
+    // 자동 재생 여부
+    delay: 3500, // 3.5초마다 슬라이드 교체
+  },
+  loop: true, // 반복 재생 여부
+  spaceBetween: 20, // 슬라이드 사이 여백
+  slidesPerView: 2, // 한 번에 보여줄 슬라이드 개수
+  slidesPerGroup: 1, // 한 번에 슬라이드 할 개수(전체 개수로 나뉘어야 함)
+  breakpoints: {
+    // when window width is >= 768px
+    1275: {
+      slidesPerView: 4.5,
+    },
+    // when window width is >= 768px
+    940: {
+      slidesPerView: 3.5,
+    },
+  },
+});
