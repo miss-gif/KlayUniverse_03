3a9142971b2676d91a68edf965f08200381eaafc
miss <ddc.akari@gmail.com>
2022-10-26 05:26:03 +0900

    modal JS

--- js/main.js
+++ js/main.js
@@ -1,6 +1,6 @@
 console.log("연결확인");
 
-/* 네비게이션 메뉴 버튼 컨트롤 */
+/* 네비게이션 메뉴 버튼 제어 */
 const navMenu = document.querySelectorAll("nav>ul>li>a");
 
 navMenu[0].addEventListener("click", function () {
@@ -57,7 +57,7 @@ navMenu[5].addEventListener("click", function () {
   navMenu[5].classList.add("active");
 });
 
-/* 탭메뉴 버튼 컨트롤 */
+/* 탭메뉴 버튼 제어 */
 
 const tabMenu = document.querySelectorAll("article>ul>li>a");
 const tabList = document.querySelectorAll(".tabList");
@@ -90,4 +90,26 @@ tabMenu[2].addEventListener("click", function (e) {
   tabList[0].classList.add("hide");
   tabList[1].classList.add("hide");
   tabList[2].classList.remove("hide");
+});
+
+/* aside 버튼을 클릭하면 모달창이 열림 */
+
+const asideBtn = document.querySelectorAll("aside>ul>li>a");
+const modalNotPage = document.querySelector(".modalNotPage");
+
+asideBtn[0].addEventListener("click", function (e) {
+  e.preventDefault();
+  modalNotPage.classList.remove("hide");
+});
+
+asideBtn[2].addEventListener("click", function (e) {
+  e.preventDefault();
+  modalNotPage.classList.remove("hide");
+});
+
+/* 모달창 닫기 */
+
+const modalClose = document.querySelector(".modalClose");
+modalClose.addEventListener("click", function () {
+  modalNotPage.classList.add("hide");
 });
